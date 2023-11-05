be700af97db5530155a0db7bff4706c80f2e6c68
Begiana123 <ana.castromarquez@students.unibe.ch>
2023-11-05 18:47:30 +0100

    adding comands to copy fastq files

---
+++ 01_read.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PATH_DATA=/data/users/acastro/rnaseq_course/breast_cancer/data/
+SOURCE=/data/courses/rnaseq_course/breastcancer_de
+
+# rm -r $PATH_DATA/reads
+
+echo "copying reads"
+cp -v -r $SOURCE $PATH_DATA
+
+# echo "uncompressing reads"
+# gunzip -k $PATH_DATA/breastcancer_de/reads/*.gz
+
+
