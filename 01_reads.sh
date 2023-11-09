dcdec1c6a8adb2a3e2c8b5cc61c3e7873f5fa750
Begiana123 <ana.castromarquez@students.unibe.ch>
2023-11-09 13:22:59 +0100

    Improve directories names

---
+++ 01_reads.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Go to working directory 
+# cd /data/users/$USER
+
+# Create directories to save reads 
+# mkdir --parents /data/users/$USER/breast_cancer/data
+
+READS_DIR=/data/users/$USER/breast_cancer/data
+
+# Copy reads locally 
+echo "copying reads"
+cp --verbose --recursive /data/courses/rnaseq_course/breastcancer_de/reads/ $READS_DIR
+
+# Unconpress reads
+# echo "uncompressing reads"
+# gunzip -k /data/courses/breast_cancer/data/reads/*.gz
+
+
