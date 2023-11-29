4f7975ccfb092492b988ac27d07ea208f092d353
Begiana123 <ana.castromarquez@students.unibe.ch>
2023-11-29 21:22:43 +0100

    Improve structure

--- 03_trimming.sh
+++ 03_trimming.sh
@@ -3,45 +3,45 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1000M
 #SBATCH --time=01:00:00
-#SBATCH --job-name=run_trimmomatic
+#SBATCH --job-name=trimmomatic
 #SBATCH --mail-user=ana.castromarquez@students.unibe.ch
 #SBATCH --mail-type=begin,end
-#SBATCH --output=/data/users/acastro/rnaseq_course/output_fastqc_%j.o
-#SBATCH --error=/data/users/acastro/rnaseq_course/error_fastqc_%j.e
+#SBATCH --output=/data/users/acastro/log/output_trim_%j.o
+#SBATCH --error=/data/users/acastro/log/error_trim_%j.e
+
+READS_DIR=/data/users/$USER/breast_cancer/data/reads
+TRIMMING_DIR=/data/users/$USER/breast_cancer/analysis/trimming
+
+# Go to working directory
+# cd /data/users/$USER
+
+# Create directories to sava results of trimming
+mkdir --parents $TRIMMING_DIR
 
 # module avail
 module add UHTS/Analysis/trimmomatic/0.36
 
-# rm --recursive $PATH_OUTPUT_TRIM
+echo "Trimming process"
 
 # List of sample names
 samples=("HER21" "HER22" "HER23" "NonTNBC1" "NonTNBC2" "NonTNBC3" "Normal1" "Normal2" "Normal3" "TNBC1" "TNBC2" "TNBC3")
 
-PATH_OUTPUT_TRIM="/data/users/acastro/rnaseq_course/breast_cancer/analysis_step/trimmed_reads"
-
-mkdir --parents "$PATH_OUTPUT_TRIM"
-
 echo "Trimming process"
 
-
 for sample in "${samples[@]}"; do
-
-    PATH_READ1="/data/users/acastro/rnaseq_course/breast_cancer/data/breastcancer_de/reads/${sample}_R1.fastq.gz"
-    PATH_READ2="/data/users/acastro/rnaseq_course/breast_cancer/data/breastcancer_de/reads/${sample}_R2.fastq.gz"
-
-    OUTPUT_FORWARD_PAIR="$PATH_OUTPUT_TRIM/${sample}_trimmed.paired_R1.fastq.gz"
-    OUTPUT_FORWARD_UNPAIR="$PATH_OUTPUT_TRIM/${sample}_trimmed.unpaired_R1.fastq.gz"
-    OUTPUT_REVERSE_PAIR="$PATH_OUTPUT_TRIM/${sample}_trimmed.paired_R2.fastq.gz"
-    OUTPUT_REVERSE_UNPAIR="$PATH_OUTPUT_TRIM/${sample}_trimmed.unpaired_R2.fastq.gz"
-
+    
+    READS_DIR_R1=/$READS_DIR/${sample}_R1.fastq.gz
+    READS_DIR_R2=/$READS_DIR/${sample}_R2.fastq.gz
+    
+    FORWARD_PAIR="$TRIMMING_DIR/${sample}_trimmed.paired_R1.fastq.gz"
+    REVERSE_PAIR="$TRIMMING_DIR/${sample}_trimmed.paired_R2.fastq.gz"
+    
     echo $sample
-
-    trimmomatic PE -threads 1 -phred33 \
-    $PATH_READ1 $PATH_READ2 \
-    $OUTPUT_FORWARD_PAIR $OUTPUT_FORWARD_UNPAIR \
-    $OUTPUT_REVERSE_PAIR $OUTPUT_REVERSE_UNPAIR \
-    LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:50
-
+    
+    trimmomatic PE -threads 1 -phred33 $READS_DIR_R1 $READS_DIR_R2 $FORWARD_PAIR $REVERSE_PAIR LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:50
+    # LEADING:20 TRAILING:20: Removes leading and trailing bases with a Phred score below 20.
+    # SLIDINGWINDOW:4:20: Performs a sliding window trimming, cutting when the average quality within the window falls below 20.
+    # MINLEN:50: Discards reads below a length of 50 bases after trimming.
+    
 done
-
 
