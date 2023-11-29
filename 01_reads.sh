5d0953f6d60dcdeec724e87533a6fe48acbb4688
Begiana123 <ana.castromarquez@students.unibe.ch>
2023-11-29 21:20:53 +0100

    Improve structure

--- 01_reads.sh
+++ 01_reads.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-# Go to working directory 
+# # Go to working directory 
 # cd /data/users/$USER
 
-# Create directories to save reads 
+# # Create directories to save reads 
 # mkdir --parents /data/users/$USER/breast_cancer/data
 
-READS_DIR=/data/users/$USER/breast_cancer/data
+# READS_DIR=/data/users/$USER/breast_cancer/data/reads
 
-# Copy reads locally 
-echo "copying reads"
-cp --verbose --recursive /data/courses/rnaseq_course/breastcancer_de/reads/ $READS_DIR
+# #Copy reads locally 
+# echo "copying reads"
+# cp --verbose --recursive /data/courses/rnaseq_course/breastcancer_de/reads/ $READS_DIR
 
 # Unconpress reads
-# echo "uncompressing reads"
-# gunzip -k /data/courses/breast_cancer/data/reads/*.gz
+echo "uncompressing reads"
+gunzip -k $READS_DIR/*.gz
 
 
