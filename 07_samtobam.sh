c43d638e3038e72f74cdf41fb51e019f28c78929
Begiana123 <ana.castromarquez@students.unibe.ch>
2023-11-30 09:15:21 +0100

    run convertion sam to bam

---
+++ 07_samtobam.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#SBATCH --array=1-12
+#SBATCH --cpus-per-task=5
+#SBATCH --mem-per-cpu=8000M
+#SBATCH --time=13:00:00
+#SBATCH --job-name=ConvertSamToBam
+#SBATCH --mail-user=ana.castromarquez@students.unibe.ch
+#SBATCH --mail-type=begin,end
+#SBATCH --output=/data/users/acastro/log/output_samtobam_%j.o
+#SBATCH --error=/data/users/acastro/log/error_samtobam_%j.e
+
+BAM_DIR=/data/users/$USER/breast_cancer/analysis/bam
+SAMPLELIST=/data/users/$USER/breast_cancer/script/samplelist.tsv
+
+# Go to working directory
+# cd /data/users/$USER
+
+SAMPLE=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $1; exit}' $SAMPLELIST`
+SAM=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $4; exit}' $SAMPLELIST`
+
+BAM_FILE="$BAM_DIR/${SAMPLE}_sorted.bam"
+
+
+############################
+
+module load UHTS/Analysis/samtools/1.10
+
+samtools view -hbS $SAM | samtools sort -o $BAM_FILE
+
+
