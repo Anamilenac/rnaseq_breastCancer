b05f29e704388992981e4910e60f907c52267a22
Begiana123 <ana.castromarquez@students.unibe.ch>
2023-12-19 15:13:59 +0100

    Adjust format and log directory

--- 04_quality_trim.sh
+++ 04_quality_trim.sh
@@ -6,14 +6,14 @@
 #SBATCH --job-name=quality_control_trim
 #SBATCH --mail-user=ana.castromarquez@students.unibe.ch
 #SBATCH --mail-type=begin,end
-#SBATCH --output=/data/users/acastro/log/output_qualitytrim_%j.o
-#SBATCH --error=/data/users/acastro/log/error_qualitytrim_%j.e
-
-TRIMMING_DIR=/data/users/$USER/breast_cancer/analysis/trimming
-QUALITY_TRIM_DIR=/data/users/$USER/breast_cancer/analysis/trimming/quality_trim
+#SBATCH --output=/data/users/acastro/breast_cancer/log/output_qualitytrim_%j.o
+#SBATCH --error=/data/users/acastro/breast_cancer/log/error_qualitytrim_%j.e
 
 # Go to working directory
 # cd /data/users/$USER
+
+TRIMMING_DIR=/data/users/$USER/breast_cancer/analysis/trimming
+QUALITY_TRIM_DIR=/data/users/$USER/breast_cancer/analysis/trimming/quality_trim
 
 # create directory to save quality control for trimmed files
 mkdir $QUALITY_TRIM_DIR
@@ -21,7 +21,7 @@ mkdir $QUALITY_TRIM_DIR
 # module avail
 module add UHTS/Quality_control/fastqc/0.11.9
 
-# Perform quality Control 
+# Perform quality Control
 echo "Analyzing quality control"
 fastqc --outdir $QUALITY_TRIM_DIR $TRIMMING_DIR/*.gz
 
