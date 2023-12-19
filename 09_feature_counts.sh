f048f6060af2b751b1006aa4a92283d56e701aee
Begiana123 <ana.castromarquez@students.unibe.ch>
2023-12-19 15:17:15 +0100

    Adjust format and log directory

--- 09_feature_counts.sh
+++ 09_feature_counts.sh
@@ -6,20 +6,20 @@
 #SBATCH --job-name=future_counts
 #SBATCH --mail-user=ana.castromarquez@students.unibe.ch
 #SBATCH --mail-type=begin,end
-#SBATCH --output=/data/users/acastro/log/output_featurecounts_%j.o
-#SBATCH --error=/data/users/acastro/log/error_featurecounts_%j.e
+#SBATCH --output=/data/users/acastro/breast_cancer/log/output_featurecounts_%j.o
+#SBATCH --error=/data/users/acastro/breast_cancer/log/error_featurecounts_%j.e
+
+# Go to working directory
+# cd /data/users/$USER
 
 BAM_DIR=/data/users/$USER/breast_cancer/analysis/bam
 REFERENCE_DIR=/data/users/$USER/breast_cancer/data/reference
 FT_COUNTS_DIR=/data/users/$USER/breast_cancer/analysis/feature_counts
 
-# Go to working directory
-# cd /data/users/$USER
-
 module load UHTS/Analysis/subread/2.0.1
 
 featureCounts -p -t exon -g gene_id -a \
-    $REFERENCE_DIR/Homo_sapiens.GRCh38.110.gtf \
-    -o $FT_COUNTS_DIR/featurecounts.txt \
-    $BAM_DIR/*_sorted.bam 
+$REFERENCE_DIR/Homo_sapiens.GRCh38.110.gtf \
+-o $FT_COUNTS_DIR/featurecounts.txt \
+$BAM_DIR/*_sorted.bam
 
