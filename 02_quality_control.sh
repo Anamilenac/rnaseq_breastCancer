55d940bb851fa14586f787dc8cd2bf12bf3ea116
Begiana123 <ana.castromarquez@students.unibe.ch>
2024-01-30 03:16:14 +0100

    update

--- 02_quality_control.sh
+++ 02_quality_control.sh
@@ -10,7 +10,7 @@
 #SBATCH --error=/data/users/acastro/breast_cancer/log/error_quality_%j.e
 
 # Go to working directory
-# cd /data/users/$USER
+# cd /data/users/$USER/breast_cancer
 
 READS_DIR=/data/users/$USER/breast_cancer/data/reads
 QUALITY_CONTROL_DIR=/data/users/$USER/breast_cancer/analysis/quality_control
